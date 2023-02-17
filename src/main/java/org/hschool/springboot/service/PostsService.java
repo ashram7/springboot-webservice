1b2d59dbd1f754d4b8728652264964d0de798610
ashram7 <ashram7@hanmail.net>
2023-02-17 18:43:45 +0900

    gitignore 재등록 및 구글로그인까지 구현

---
+++ src/main/java/org/hschool/springboot/service/PostsService.java
@@ -0,0 +1,55 @@
+package org.hschool.springboot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.hschool.springboot.domain.posts.Posts;
+import org.hschool.springboot.domain.posts.PostsRepository;
+import org.hschool.springboot.web.dto.PostsListResponseDto;
+import org.hschool.springboot.web.dto.PostsResponseDto;
+import org.hschool.springboot.web.dto.PostsSaveRequestDto;
+import org.hschool.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts); // 1)
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id"+ id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findeAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
